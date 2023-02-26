a344d448905cd2269fcb569553145f800eeaa7b3
kodtld <kodu@live.fi>
2023-02-26 10:25:37 +0200

    New tests for markov_service

--- flask-server/tests/markov_test.py
+++ flask-server/tests/markov_test.py
@@ -11,16 +11,17 @@ class TestMarkovChain(unittest.TestCase):
     """
     def setUp(self):
         self.trie = Trie(1)
-        self.trie.insert("On the other hand, we denounce with righteous indignation and dislike")
-        self.trie.insert("that they cannot foresee the pain and trouble that are bound to")
+        self.trie.insert("On the other hand, we denounce with righteous hate and dislike")
+        self.trie.insert("That they cannot foresee the pain and trouble that are bound to")
+        self.trie.insert("That they cannot on the mat puke unfortunately")
         self.trie.insert("Word is a bird")
-
+        self.trie.insert("The cat in the hat sat on in a hat")
+        self.markov_chain = MarkovChain(self.trie)
     def test_generate_sentence(self):
         """
         Test if generate_sentence returns a valid sentence
         """
-        markov_chain = MarkovChain(self.trie)
-        result = markov_chain.generate_sentence(max_length=10)
+        result = self.markov_chain.generate_sentence(max_length=10)
         self.assertIsInstance(result, str)
         self.assertTrue(1 <= len(result.split()) <= 10)
 
@@ -28,8 +29,7 @@ class TestMarkovChain(unittest.TestCase):
         """
         Test if generate_sentence starts with the specified starting word
         """
-        markov_chain = MarkovChain(self.trie)
-        result = markov_chain.generate_sentence(starting_word="word", max_length=10)
+        result = self.markov_chain.generate_sentence(starting_word="word", max_length=10)
         print(result)
         self.assertTrue(result.startswith("Word"))
 
@@ -37,8 +37,7 @@ class TestMarkovChain(unittest.TestCase):
         """
         Test if generate_two_sentence with no starting bigram returns a string of the expected length
         """
-        markov_chain = MarkovChain(self.trie)
-        result = markov_chain.generate_sentence(max_length=10)
+        result = self.markov_chain.generate_sentence(max_length=10)
         self.assertIsInstance(result, str)
         self.assertTrue(2 <= len(result.split()) <= 10)
 
@@ -46,11 +45,30 @@ class TestMarkovChain(unittest.TestCase):
         """
         Test if generate_two_sentence with a starting bigram returns a string of the expected length
         """
-        markov_chain = MarkovChain(self.trie)
-        result = markov_chain.generate_sentence(starting_word="that they",max_length=10)
+        result = self.markov_chain.generate_sentence(starting_word="that they",max_length=10)
         self.assertIsInstance(result, str)
         self.assertTrue(2 <= len(result.split()) <= 10)
+    
+    def test_handle_starting_prompt_greater_or_equal(self):
+        """
+        Test the handling of a propmpt greater or equal in length to the degree
+        """
+        result = self.markov_chain.handle_starting_prompt("the cat in", 2)
+        self.assertEqual(result["current_ngram"], "cat in")
+        self.assertEqual(result["sentence"], ["the", "cat", "in"])
 
+    def test_handle_starting_prompt_shorter(self):
+        """
+        Test the handling of a propmpt shorter in length to the degree
+        """
+        self.trie = Trie(3)
+        self.trie.insert("On the other hand, we denounce with righteous hate and dislike")
+        self.trie.insert("that they cannot foresee the pain and trouble that are bound to")
+        self.trie.insert("Word is a bird")
+        self.trie.insert("The cat in the hat sat on in a hat")
+        self.markov_chain = MarkovChain(self.trie)
+        result = self.markov_chain.handle_starting_prompt("in", 2)
+        self.assertIn(result["sentence"][0], ["in", "the", "hat", "mat"])
 
 if __name__ == '__main__':
     unittest.main()
