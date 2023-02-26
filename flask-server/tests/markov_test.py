716b34a40944d8c630e180080a717cc7cf2e2664
kodtld <kodu@live.fi>
2023-02-26 10:08:30 +0200

    New tests for markov_service

--- flask-server/tests/markov_test.py
+++ flask-server/tests/markov_test.py
@@ -10,46 +10,46 @@ class TestMarkovChain(unittest.TestCase):
     Includes the tests for the Markov Chain
     """
     def setUp(self):
-        self.trie = Trie(2)
+        self.trie = Trie(1)
         self.trie.insert("On the other hand, we denounce with righteous indignation and dislike")
         self.trie.insert("that they cannot foresee the pain and trouble that are bound to")
         self.trie.insert("Word is a bird")
 
-    # def test_generate_sentence(self):
-    #     """
-    #     Test if generate_sentence returns a valid sentence
-    #     """
-    #     markov_chain = MarkovChain(self.trie)
-    #     result = markov_chain.generate_sentence(max_length=10)
-    #     self.assertIsInstance(result, str)
-    #     self.assertTrue(1 <= len(result.split()) <= 10)
+    def test_generate_sentence(self):
+        """
+        Test if generate_sentence returns a valid sentence
+        """
+        markov_chain = MarkovChain(self.trie)
+        result = markov_chain.generate_sentence(max_length=10)
+        self.assertIsInstance(result, str)
+        self.assertTrue(1 <= len(result.split()) <= 10)
 
-    # def test_generate_sentence_starting_word(self):
-    #     """
-    #     Test if generate_sentence starts with the specified starting word
-    #     """
-    #     markov_chain = MarkovChain(self.trie)
-    #     result = markov_chain.generate_sentence(starting_word="word", max_length=10)
-    #     print(result)
-    #     self.assertTrue(result.startswith("Word"))
+    def test_generate_sentence_starting_word(self):
+        """
+        Test if generate_sentence starts with the specified starting word
+        """
+        markov_chain = MarkovChain(self.trie)
+        result = markov_chain.generate_sentence(starting_word="word", max_length=10)
+        print(result)
+        self.assertTrue(result.startswith("Word"))
 
-    # # def test_generate_two_sentence_no_bigram(self):
-    # #     """
-    # #     Test if generate_two_sentence with no starting bigram returns a string of the expected length
-    # #     """
-    # #     markov_chain = MarkovChain(self.trie)
-    # #     result = markov_chain.generate_sentence(max_length=10)
-    # #     self.assertIsInstance(result, str)
-    # #     self.assertTrue(2 <= len(result.split()) <= 10)
+    def test_generate_two_sentence_no_bigram(self):
+        """
+        Test if generate_two_sentence with no starting bigram returns a string of the expected length
+        """
+        markov_chain = MarkovChain(self.trie)
+        result = markov_chain.generate_sentence(max_length=10)
+        self.assertIsInstance(result, str)
+        self.assertTrue(2 <= len(result.split()) <= 10)
 
-    # # def test_generate_two_sentence_set_bigram(self):
-    # #     """
-    # #     Test if generate_two_sentence with a starting bigram returns a string of the expected length
-    # #     """
-    # #     markov_chain = MarkovChain(self.trie)
-    # #     result = markov_chain.generate_sentence(starting_word="that they",max_length=10)
-    # #     self.assertIsInstance(result, str)
-    # #     self.assertTrue(2 <= len(result.split()) <= 10)
+    def test_generate_two_sentence_set_bigram(self):
+        """
+        Test if generate_two_sentence with a starting bigram returns a string of the expected length
+        """
+        markov_chain = MarkovChain(self.trie)
+        result = markov_chain.generate_sentence(starting_word="that they",max_length=10)
+        self.assertIsInstance(result, str)
+        self.assertTrue(2 <= len(result.split()) <= 10)
 
 
 if __name__ == '__main__':
